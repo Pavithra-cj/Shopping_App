767bfef4c8e02a2f95f2ac1a094d6d8c67e3b464
Pavithra Chamod J <84024015+Pavithra-cj@users.noreply.github.com>
2023-09-04 14:53:11 +0530

    Main activity change back to Home.

--- ShoppingApp/src/Navigation/index.js
+++ ShoppingApp/src/Navigation/index.js
@@ -1,11 +1,21 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import {NavigationContainer} from '@react-navigation/native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import Login from '../Screen/Auth/Login';
+import Register from '../Screen/Auth/Register';
+import MyTabs from '../components/TabNavigation';
+import HeaderBar from '../components/HeaderBar';
+
+const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <Text>Navigation</Text>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name='SignIn' component={Login} />
+        <Stack.Screen name='Register' component={Register} />
+      </Stack.Navigator>
     </NavigationContainer>
   )
 }
